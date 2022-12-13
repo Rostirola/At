f3fdb6c5571bd555ab0c00980caf87944a10199b
Juan Rostirola <juanvitorrostirola666@gmail.com>
2022-12-13 20:09:03 -0300

    Implementacao classe mae e uma classe filha

--- src/main/java/br/edu/infnet/appsorvete/model/domain/Alimento.java
+++ src/main/java/br/edu/infnet/appsorvete/model/domain/Alimento.java
@@ -6,4 +6,41 @@ public abstract class Alimento {
 	private String sabor;
 	private int quantidade;
 	
+	public Alimento(float preco, String sabor, int quantidade) {
+		this.preco = preco;
+		this.sabor = sabor;
+		this.quantidade = quantidade;
+	}
+	
+	public abstract float calcularValorVenda();
+	
+	@Override
+	public String toString() {
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(preco);
+		sb.append(";");
+		sb.append(sabor);
+		sb.append(";");
+		sb.append(quantidade);
+		sb.append(";");
+		
+		return sb.toString();
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public String getSabor() {
+		return sabor;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+	
+	
+	
+	
 }
--- src/main/java/br/edu/infnet/appsorvete/model/domain/Bebida.java
+++ src/main/java/br/edu/infnet/appsorvete/model/domain/Bebida.java
@@ -2,8 +2,62 @@ package br.edu.infnet.appsorvete.model.domain;
 
 public class Bebida extends Alimento {
 
+	
 	private String marca;
 	private int tamanho;
 	private boolean alcoolico;
+	
+	public Bebida(float preco, String sabor, int quantidade, String marca) {
+		super(preco, sabor, quantidade);
+		this.marca = marca;
+	}
+	
+	@Override
+	public float calcularValorVenda() {
+		
+		float valorAlcoolico = alcoolico ? 3 : 0;
+		float valorTamanho = tamanho * 0.05f;
+		
+		return getPreco() + valorAlcoolico + valorTamanho;
+	}
+	
+	@Override
+	public String toString() {
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(super.toString());
+		sb.append(";");
+		sb.append(marca);
+		sb.append(";");
+		sb.append(tamanho);
+		sb.append(";");
+		sb.append(alcoolico);
+		sb.append(";");
+		
+		return sb.toString();
+	}
+	
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public int getTamanho() {
+		return tamanho;
+	}
+	public void setTamanho(int tamanho) {
+		this.tamanho = tamanho;
+	}
+	public boolean isAlcoolico() {
+		return alcoolico;
+	}
+	public void setAlcoolico(boolean alcoolico) {
+		this.alcoolico = alcoolico;
+	}
+
+	
+	
+	
 	
 }
